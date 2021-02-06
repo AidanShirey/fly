ca6d9724d537f630d74a120bf98fc3f74b68a90e
Aidan Shirey <aidan.shirey@gmail.com>
2021-02-05 18:18:17 -0800

    zip picture update

--- read.js
+++ read.js
@@ -5,14 +5,34 @@ var fileP;
 
 
 // Code for outputting file content
-function previewZipFile(value)
+function previewZipFile(value, name)
 {
-    var lines = value.split('\n');
-    if (lines.length > 1){
+    var extension = name.substr(name.lastIndexOf('.') + 1);
+    if (extension == 'txt'){
+        var lines = value.split('\n');
+        if (lines.length > 1){
         for(var i = 0; i < lines.length; i++)
             document.getElementById("preview").innerHTML += value;
+        }
+        document.getElementById("preview").innerHTML = value;
     }
-    document.getElementById("preview").innerHTML = value;                 
+    else if (extension == 'gif' || extension == 'png' || extension == 'jpg'){
+        var $img = document.createElement("img");
+        $img.src = value;
+        var preview = document.getElementById("preview");
+        while (preview.firstChild){
+            preview.removeChild(preview.firstChild);
+        }
+        document.getElementById("preview").append($img);
+    }
+    else {
+        var lines = value.split('\n');
+        if (lines.length > 1){
+        for(var i = 0; i < lines.length; i++)
+            document.getElementById("preview").innerHTML += value;
+        }
+        document.getElementById("preview").innerHTML = value;
+    }          
 }
 
 
@@ -46,7 +66,10 @@ $("#file").on("change", function(evt) {
 
             zip.forEach(function (relativePath, zipEntry) {  // 2) print entries
                 if (zipEntry.dir != true){
-                    zip.file(zipEntry.name).async('string').then(function (fileData) {
+                    var filename = zipEntry.name;
+                    var extension = filename.substr(filename.lastIndexOf('.') + 1);
+                    if (extension == 'txt'){
+                        zip.file(zipEntry.name).async('string').then(function (fileData) {
                         // fileData is a string of the contents
                         var content = fileData.split("\n");
                         content = content.slice(0, content.length - 1);
@@ -59,12 +82,33 @@ $("#file").on("change", function(evt) {
                         });
                         var $previewButton = $("<button>",{
                             text : "preview",
-                            "onclick": "previewZipFile('"+ content +"')"
+                            "onclick": "previewZipFile('"+ content +"','" + zipEntry.name + "')"
                         });
                         $filecol.append($previewButton);
                         $rowContent.append($filecol); 
                     });
                     filecount++;
+                    }
+                    else if (extension == 'gif' || extension == 'png' || extension == 'jpg'){
+                        zip.file(zipEntry.name).async('base64').then(function (fileData) {
+                        // fileData is a string of the contents
+                            var content = "data:blob;base64," + fileData;
+                            var $filecol = $("<div>", {
+                                "id": zipEntry.name,
+                                "class": "column",
+                                "value": content,
+                                "style": "background-color:#bbb;",
+                                text : zipEntry.name
+                            });
+                            var $previewButton = $("<button>",{
+                                text : "preview",
+                                "onclick": "previewZipFile('"+ content +"','" + zipEntry.name + "')"
+                            });
+                            $filecol.append($previewButton);
+                            $rowContent.append($filecol); 
+                        });
+                        filecount++;
+                    }
                 }
             });
 
@@ -91,6 +135,7 @@ $("#file").on("change", function(evt) {
         {
             return function()
             {
+                
                 var content = reader.result;
                 var lines = content.split('\n');
                 var dateBefore = new Date();
